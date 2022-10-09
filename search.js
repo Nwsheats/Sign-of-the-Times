1c09eaee5be4d6d30bec38966f0a17c7fa0ad33e
C. Donnelly <donn0021@umn.edu>
2022-10-09 16:54:03 -0500

    added byeabbe fetch results success

--- search.js
+++ search.js
@@ -1,5 +1,30 @@
 let bDate = 1;
-let month = 4;
-var day = 6;
+let month = 5;
+var day = 9;
 var byabbeURI = "https://byabbe.se/on-this-day/"
 
+fetch('https://byabbe.se/on-this-day/'+month+'/'+day+'/births.json', {
+// ) {
+//   method: 'GET', //GET is the default.
+//   // appid: 'bdc63249ca9c768d065db41d5ee05a7d', // apikey
+//   // q: 'Minneapolis', // Query to search for
+// 
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+// fetch(byabbeURI+bDate+month) {
+//   method: 'GET', //GET is the default.
+//   // appid: 'bdc63249ca9c768d065db41d5ee05a7d', // apikey
+//   // q: 'Minneapolis', // Query to search for
+// })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
